36b176af49ebec2611303c99e16d28c506fd8b81
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-10-29 22:26:07 +0300

    add data.json

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
-  <div className="wrapper">
-    <div className="card">=)</div>
-    <div className="card">O_o</div>
-    <div className="card">^_^</div>
-    <div className="card">=(</div>
-
-    <h1>Hello Application!</h1>
-  </div>,
-  document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
