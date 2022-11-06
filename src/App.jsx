b682b1a1a8a1d239535138e05587783e4ecded60
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-11-06 20:05:57 +0200

    Add Product Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import React, { useState } from "react";
-import Card from "./components/Card";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Product from "./pages/Product";
+// import Catalog from "./pages/Catalog";
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { Container, Row, Col } from "react-bootstrap";
 
-import data from "./assets/data.json"
 
 
 const App = () => {
-    const [goods, setGoods] = useState(data);
+    {/*const st = {
+        height: "50px",
+        backgroundColor: "silver"
+    }
 
-    return (
-        <div className="wrapper">
-            <Header products={goods} />
-            <div className="cards-container">
-                {/* <Card/> */}
-                {data.map((d, i) => <Card
-                    key={i}
-                    img={d.picture}
-                    text={d.name}
-                    price={d.price}
-                />)}
-            </div>
-            <Footer />
-        </div>
-    )
+    return <Container style={{ height: "900px", backgroundColor: "darkorchid" }}>
+         <Row>
+            <Col xs={12} style={st} />
+            <Col md={3} xs={6} style={st} />
+            <Col md={3} xs={6} style={st} />
+            <Col md={3} xs={6} style={st} />
+            <Col md={3} xs={6} style={st} />
+            <Col xs={12} style={st} />
+            <Col xs={6} style={st} />
+            <Col xs={6} style={st} />
+            <Col xs={6} style={st} />
+            <Col xs={12} style={st} />
+        </Row> 
+    </Container >*/}
+    return <Product />
 }
 
 export default App;
---
+++ src/pages/Product.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import data from "../assets/data.json"
+import { Container, Row, Col, Figure, Table, ButtonGroup, Button, Alert } from "react-bootstrap";
+import { Truck } from "react-bootstrap-icons";
+
+export default () => {
+   let p = data[0]
+   const [cnt, setCnt] = useState(0);
+   return <Container>
+
+      <Row>
+         <Col xs={12}>
+            <h1>{p.name}</h1>
+         </Col>
+         <Col xs={12} md={8}>
+            <Figure>
+               <Figure.Image src={p.picture} />
+            </Figure>
+         </Col>
+         <Col xs={12} md={4}>
+            {p.discount && <small><del>{p.price} ₽ </del></small>}
+            <div><strong className={p.discount ? "text-danger" : "text-dark"}>{Math.ceil(p.price * ((100 - p.discount) / 100))}</strong></div>
+            <Row>
+               <Col md={6}>
+                  <ButtonGroup>
+                     <Button size="sm" variant="light" disabled={!cnt} onClick={e => setCnt(cnt - 1)}>-</Button>
+                     <Button size="sm" variant="light" disabled>{cnt}</Button>
+                     <Button size="sm" variant="light" onClick={e => setCnt(cnt + 1)}>+</Button>
+                  </ButtonGroup>
+               </Col>
+               <Col md={6}>
+                  <Button size="sm" variant="warning">В корзину</Button>
+               </Col>
+            </Row>
+            <Alert variant="secondary" styleName="mt-3">
+               <Row>
+                  <Col md={1}><Truck /></Col>
+                  <Col><small>Доставка по всему миру!</small></Col>
+               </Row >
+            </Alert >
+         </Col >
+         <Col xs={12}>
+            <h2>Описание</h2>
+            <p>{p.description}</p>
+         </Col>
+         <Col xs={12}>
+            <h2>Характеристики</h2>
+            <Table hover>
+               <tbody>
+                  <tr>
+                     <th>Вес</th>
+                     <th>{p.wight} грамм</th>
+                  </tr>
+                  <tr>
+                     <th>Цена</th>
+                     <th>{p.price} ₽ за 100 грамм</th>
+                  </tr>
+                  <tr>
+                     <th>Польза</th>
+                     <th>{p.description}</th>
+                  </tr>
+               </tbody>
+            </Table>
+         </Col>
+         <Col xs={12}></Col>
+
+
+      </Row >
+   </Container >
+}
