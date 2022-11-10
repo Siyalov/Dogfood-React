c0b55d814be6a851954af1f24b170edb22e27e78
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-11-10 21:50:16 +0200

    api methods added

--- src/Api.js
+++ src/Api.js
@@ -1,28 +1,76 @@
 class Api {
-   constructor(t){
-   this.path = "https://api.react-learning.ru";
-   this.token = t;
-   }
-   getProducts() {
-      return fetch(`${this.path}/products`, {
-          headers: {
-              "Authorization": `Bearer ${this.token}`
-          }
-      })
-      .then(res => res.json())
-      .then(data => data);
-   }   
-   getProduct() {
-   }
-   addProduct() {
-   }
-   updProduct() {
-   }
-   delProduct() {
-   }
-   logIn(){
-   }
-   singUp(){
-   }
+    constructor(t) {
+        this.path = "https://api.react-learning.ru";
+        this.token = t;
+    }
+    getProducts() {
+        return fetch(`${this.path}/products`, {
+            method: 'GET',
+            headers: {
+                "Authorization": `Bearer ${this.token}`
+            }
+        })
+            .then(res => res.json());
+    }
+    getProduct(id) {
+        return fetch(`${this.path}/products/${id}`, {
+            method: 'GET',
+            headers: {
+                "Authorization": `Bearer ${this.token}`
+            }
+        })
+            .then(res => res.json());
+    }
+    addProduct(body) {
+        return fetch(`${this.path}/products`, {
+            method: 'POST',
+            headers: {
+                "Authorization": `Bearer ${this.token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        })
+            .then(res => res.json());
+    }
+    updProduct(id, body) {
+        return fetch(`${this.path}/products/${id}`, {
+            method: 'PATCH',
+            headers: {
+                "Authorization": `Bearer ${this.token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        })
+            .then(res => res.json());
+    }
+    delProduct(id) {
+        return fetch(`${this.path}/products/${id}`, {
+            method: 'DELETE',
+            headers: {
+                "Authorization": `Bearer ${this.token}`
+            },
+        })
+            .then(res => res.json());
+    }
+    logIn(body) {
+        return fetch(`${this.path}/signin`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        })
+            .then(res => res.json());
+    }
+    singUp(body) {
+        return fetch(`${this.path}/signup`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        })
+            .then(res => res.json());
+    }
 }
 export default Api;
