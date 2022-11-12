f82f5790a6fa51d0970d331d35a747da6da8e567
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-11-12 17:00:10 +0200

    AdCard added on page

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "../components/Card";
 
-import data from "../assets/data.json"
+import AdCard from "../components/AdCard";
 
 export default ({ goods }) => {
    return <div className="cards-container">
-      {goods.map((d, i) => <Card
-         key={i}
-         img={d.pictures}
-         text={d.name}
-         price={d.price}
-      />)}
+      {goods.map((d, i) => {
+         return <>
+            {i % 8 == 0 ? <AdCard text={<>
+               <h2>Подарок за<br />первый заказ!</h2>
+               <span>{d.name}</span>
+            </>} img={d.pictures} /> : ''}
+            <Card
+               key={i}
+               img={d.pictures}
+               text={d.name}
+               price={d.price}
+            />
+         </>
+      })}
    </div>
 }
