e29e3a01f436b884ece7bcc7d9aee0cfb48e78f0
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-11-12 21:55:27 +0200

    catalog: empty message added

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -5,19 +5,22 @@ import AdCard from "../components/AdCard";
 
 export default ({ goods }) => {
    return <div className="cards-container">
-      {goods.map((d, i) => {
-         return <>
-            {i % 8 == 0 ? <AdCard text={<>
-               <h2>Подарок за<br />первый заказ!</h2>
-               <span>{d.name}</span>
-            </>} img={d.pictures} /> : ''}
-            <Card
-               key={i}
-               img={d.pictures}
-               text={d.name}
-               price={d.price}
-            />
-         </>
-      })}
+      {goods?.length > 0 ?
+         goods.map((d, i) => {
+            return <>
+               {i % 8 == 0 ? <AdCard text={<>
+                  <h2>Подарок за<br />первый заказ!</h2>
+                  <span>{d.name}</span>
+               </>} img={d.pictures} /> : ''}
+               <Card
+                  key={i}
+                  img={d.pictures}
+                  text={d.name}
+                  price={d.price}
+               />
+            </>
+         })
+         : <p style={{ gridColumnEnd: "span 4", textAlign: "center" }}>Для отображения данных необходимо войти в приложение</p>
+      }
    </div>
 }
