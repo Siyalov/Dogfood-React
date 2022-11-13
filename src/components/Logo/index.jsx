7bdbbc88581a9237258fe3ee534df3e91117f26b
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-11-13 22:44:30 +0200

    small logo version added

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,11 +1,13 @@
 
 import React from "react";
 import logo from './img/logo.svg'
+import miniLogo from './img/mini-logo.svg'
+import "./style.css"
 
 
 export default () => {
    return <a className="logo" href="">
-      <img src={logo} alt="Dog Food" />
-
+      <img src={logo} alt="Dog Food" className="full-logo" />
+      <img src={miniLogo} alt="Dog Food" className="small-logo" />
    </a>
 }
