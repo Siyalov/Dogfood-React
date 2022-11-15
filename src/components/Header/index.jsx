ddcb1758ba533b02ef6aae42269e22be2a12af15
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-11-16 01:45:59 +0200

    add routes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Logo from "../Logo"
 import { BoxArrowInRight, BoxArrowLeft } from "react-bootstrap-icons";
 import "./style.css";
@@ -6,7 +7,7 @@ import { ReactComponent as FavIcon } from './img/ic-favorites.svg'
 import { ReactComponent as CartIcon } from './img/ic-cart.svg'
 import { ReactComponent as ProfileIcon } from './img/ic-profile.svg'
 
-export default ({ products, update, openPopup, user, setToken }) => {
+export default ({ products, update, openPopup, user, setToken, setUser }) => {
    const [text, changeText] = useState("");
    const [cnt, setCnt] = useState(0);
    const handler = e => {
@@ -22,16 +23,19 @@ export default ({ products, update, openPopup, user, setToken }) => {
    const logout = e => {
       e.preventDefault();
       localStorage.removeItem("shop-user");
-      setToken(false);
+      localStorage.removeItem("user");
+      setToken("");
+      setUser({});
    }
+
    return <>
       <header>
          <Logo />
          <input type="search" value={text} onChange={handler} />
          <nav>
-            {user && <a href=""><FavIcon /></a>}
-            {user && <a href=""><CartIcon /></a>}
-            {user && <a href=""><ProfileIcon /></a>}
+            {user && <a href="/"><FavIcon /></a>}
+            {user && <Link to="/catalog"><CartIcon /></Link>}
+            {user && <Link to="/profile"><ProfileIcon /></Link>}
             {user && <a href="" onClick={logout}><BoxArrowLeft style={{ fontSize: "1.6rem" }} /></a>}
             {!user && <a href="" onClick={e => { e.preventDefault(); openPopup(true) }}><BoxArrowInRight style={{ fontSize: "1.6rem" }} /></a>}
          </nav>
