7ce48ef665bc5e725e2fc7de8057646d95e142b2
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-11-24 01:15:36 +0200

    catalog header added

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -6,30 +6,38 @@ import AdCard from "../components/AdCard";
 export default ({ goods, api, setFav, user }) => {
    // const myLikes = goods.filter((e) => e.likes.includes(user._id));
    // console.log(myLikes.length, myLikes)
-   return <div className="cards-container">
-      {goods?.length > 0 ?
-         goods.map((d, i) => {
-            return <>
-               {i % 8 == 0 && !user?._id ?
-                  <AdCard
-                     key={'ad' + i}
-                     text={<>
-                        <h2>Подарок за<br />первый заказ!</h2>
-                        <span>{d.name}</span>
-                     </>}
-                     img={d.pictures}
-                     cardColor={`rgba(${Math.random() * 256},${Math.random() * 256},${Math.random() * 256}, 0.6)`}
-                  /> : ''}
-               <Card
-                  key={i}
-                  {...d}
-                  api={api}
-                  setFav={setFav}
-                  userId={user?._id}
-               />
-            </>
-         })
-         : <p key='empty' style={{ gridColumnEnd: "span 4", textAlign: "center" }}>Для отображения данных необходимо войти в приложение</p>
-      }
-   </div>
+   return <>
+      <div className="main-page-header">
+         <div className="h-wrapper">
+            <h1><b>Крафтовые лакомства для собак</b></h1>
+            <h4>Всегда свежие лакомства ручной работы с доставкой по России и миру</h4>
+         </div>
+      </div>
+      <div className="cards-container">
+         {goods?.length > 0 ?
+            goods.map((d, i) => {
+               return <>
+                  {i % 8 == 0 && !user?._id ?
+                     <AdCard
+                        key={'ad' + i}
+                        text={<>
+                           <h2>Подарок за<br />первый заказ!</h2>
+                           <span>{d.name}</span>
+                        </>}
+                        img={d.pictures}
+                        cardColor={`rgba(${Math.random() * 256},${Math.random() * 256},${Math.random() * 256}, 0.6)`}
+                     /> : ''}
+                  <Card
+                     key={i}
+                     {...d}
+                     api={api}
+                     setFav={setFav}
+                     userId={user?._id}
+                  />
+               </>
+            })
+            : <p key='empty' style={{ gridColumnEnd: "span 4", textAlign: "center" }}>Для отображения данных необходимо войти в приложение</p>
+         }
+      </div>
+   </>
 }
