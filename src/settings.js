6400f1d5c13cf8e282b41c8304ee2aa10e5c52cf
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-11-24 02:00:20 +0200

    routing fix 4, rebuild

--- src/settings.js
+++ src/settings.js
@@ -1,2 +1,2 @@
-export const path = location.href.includes('github.com') ? '/Dogfoot-React/' : '/';
+export const path = location.href.includes('github.') ? '/Dogfoot-React/' : '/';
 export const defaultToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzdlOWU5ODU5Yjk4YjAzOGY3NzlkNDciLCJncm91cCI6InB1YmxpYy1ncm91cCIsImlhdCI6MTY2OTI0MjczMiwiZXhwIjoxNzAwNzc4NzMyfQ.LBNBuL9jONfQwkaeQPI4efNCW14nhmKVeyQ3IbiWNpg';
