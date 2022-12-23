c85e4a89d76fe1adfd486b6172d25641301b9e60
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-12-23 18:43:20 +0200

    Logo fix (#5)

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,14 +1,15 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from './img/logo.svg';
-import miniLogo from './img/mini-logo.svg';
-import "./style.css"
+import logo from "./img/logo.svg";
+import miniLogo from "./img/mini-logo.svg";
+import "./style.css";
 import { path } from "../../settings";
 
 export default () => {
-   return <Link className="logo" to={path}>
+  return (
+    <Link className="logo" to={path}>
       <img src={logo} alt="Dog Food" className="full-logo" />
       <img src={miniLogo} alt="Dog Food" className="small-logo" />
-   </Link>
-}
+    </Link>
+  );
+};
